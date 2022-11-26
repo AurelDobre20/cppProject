ab3d3ee5490d42354cb86a2ab201e4e971a23d6e
Dobre Aurel-Ninel <101126905+AurelDobre20@users.noreply.github.com>
2022-11-27 00:23:45 +0200

    Revert "The class Event was created."
    This reverts commit 88ca9d47d0036b68669e8fb631c11ed5f88858d1.

--- MyProject/Main.cpp
+++ MyProject/Main.cpp
@@ -1,14 +1,7 @@
 #include<iostream>
-#include"Event.cpp"
 
 using namespace std;
 
 int main() {
 	cout << "Hello world" << endl;
-
-	Event e("11/22/2022", "22:10", "Loca");
-
-	cout << e.getDate() << endl;
-	cout << e.getTime() << endl;
-	cout << e.getName() << endl;
 }
