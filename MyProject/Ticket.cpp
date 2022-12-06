c22a6614cb90d3da360cb7628133848cfe63c7cc
Dobre Aurel-Ninel <101126905+AurelDobre20@users.noreply.github.com>
2022-12-06 21:03:26 +0200

    Add getters and setters for all attributes

--- MyProject/Ticket.cpp
+++ MyProject/Ticket.cpp
@@ -45,6 +45,57 @@ public:
 		boxes = nullptr;
 	}
 
+	int getId() {
+		return this->id;
+	}
+
+	void setBoxes(int* boxes, int noBoxes) {
+		if (boxes != nullptr && noBoxes > 0) {
+			this->boxes = new int[noBoxes];
+			for (int i = 0; i < noBoxes; i++) {
+				this->boxes[i] = boxes[i];
+			}
+		}
+		this->noBoxes = noBoxes;
+	}
+
+	int* getBoxes() {
+		if (this->boxes != nullptr && this->noBoxes > 0) {
+
+			int* copy = new int[noBoxes];
+			for (int i = 0; i < this->noBoxes; i++) {
+				copy[i] = this->boxes[i];
+			}
+			return copy;
+		}
+	}
+
+	void setVIP(bool value) {
+		this->VIP = value;
+	}
+	bool getVIP() {
+		return this->VIP;
+	}
+
+	void setLawn(int value) {
+		if (value > 0 && value < 4) {
+			this->lawn = (LAWN)value;
+		}
+	}
+	string getLawn() {
+		if (this->lawn == 1) {
+			return "Lawn 1" ;
+		}
+		if (this->lawn == 2) {
+			return "Lawn 2";
+		}
+		if (this->lawn == 3) {
+			return "Lawn 3";
+		}
+	}
+
+
+
 
 
 
